5eba0c5be8e6be2391c3554226cf99d53c207450
Попов Сергей Владимирович <serge@office.lan>
2017-11-27 17:41:03 +0700

    test delay caused by blynk

--- src/project01.lua
+++ src/project01.lua
@@ -1,6 +1,38 @@
-print("Hello World!")
+
 dofile('lib-BLYNK.lua')
+
+PROFILE_TICK = 5 --ms
+HEARTBEAT_TICK = 10000 --ms
+
+lastTime = -1
+maxDelay = -1
+
+function performanceTick()
+  currentTime = tmr.now()
+  if lastTime<0 then
+    lastTime = currentTime
+  else
+    currentDelay = bit.band((currentTime - lastTime), 0x7fffffff) --compensates for timer rollover https://github.com/hackhitchin/esp8266-co-uk/issues/2
+    lastTime = currentTime
+    if currentDelay>maxDelay then
+      maxDelay = currentDelay
+    end
+  end
+end
+
+function heartBeat()
+  print("HB: maxDelay: ", maxDelay)
+  maxDelay = -1
+end
+
+print("Hello World!")
+
+tmr.create():alarm(PROFILE_TICK, tmr.ALARM_AUTO, performanceTick)
+tmr.create():alarm(HEARTBEAT_TICK, tmr.ALARM_AUTO, heartBeat)
+
 bl = blynk.new(MyBlynkToken) --defined in creds.lua
 print("Before connect")
 bl:connect()
 
+performanceTick()
+heartBeat()
